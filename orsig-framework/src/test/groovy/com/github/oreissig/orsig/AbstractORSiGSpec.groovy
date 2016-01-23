bc90433d5dbfaff3ca90625fb98eb5e51e59900c
oreissig <oreissig+github@gmail.com>
2016-01-23 15:42:48 +0100

    print test names

--- orsig-framework/src/test/groovy/com/github/oreissig/orsig/AbstractORSiGSpec.groovy
+++ orsig-framework/src/test/groovy/com/github/oreissig/orsig/AbstractORSiGSpec.groovy
@@ -2,6 +2,8 @@ package com.github.oreissig.orsig
 
 import groovy.transform.CompileStatic
 
+import org.junit.Rule
+import org.junit.rules.TestName
 import org.osgi.framework.Bundle
 import org.osgi.framework.launch.Framework
 
@@ -24,4 +26,11 @@ class AbstractORSiGSpec extends Specification {
     }
     
     final Framework framework = new ORSiGFramework()
+    
+    @Rule
+    final TestName testName = new TestName()
+    
+    def setup() {
+        println "\n-> Test $testName.methodName:"
+    }
 }
