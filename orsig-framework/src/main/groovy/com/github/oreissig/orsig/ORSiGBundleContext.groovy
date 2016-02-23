c64078736a57bb4959cb33ba749f65233bdc01be
oreissig <oreissig+github@gmail.com>
2016-02-23 18:42:27 +0100

    base for event handling

--- orsig-framework/src/main/groovy/com/github/oreissig/orsig/ORSiGBundleContext.groovy
+++ orsig-framework/src/main/groovy/com/github/oreissig/orsig/ORSiGBundleContext.groovy
@@ -11,10 +11,18 @@ import com.github.oreissig.orsig.dummy.DummyBundleContext
 class ORSiGBundleContext extends DummyBundleContext {
     @Override
     Bundle installBundle(String location, InputStream input = null) throws BundleException {
+        // TODO 1. If a bundle containing the same location identifier is already installed, the Bundle object for that bundle is returned.
+        // 2. The bundle's content is read from the input stream. If this fails, a BundleException is thrown.
         if (input)
             throw new BundleException('installBundle with explicit input not supported yet',
                                       BundleException.READ_ERROR)
-        
-        return new ORSiGBundle(location)
+        def bundle = new ORSiGBundle(location)
+        // TODO 3. The bundle's associated resources are allocated. The associated resources minimally consist of a unique identifier and a persistent storage area if the platform has file system support. If this step fails, a BundleException is thrown.
+        // 4. The bundle's state is set to INSTALLED.
+        // somehow "bundle.state =" doesn't work
+        bundle.setState Bundle.INSTALLED
+        // TODO 5. A bundle event of type BundleEvent.INSTALLED is fired.
+        // 6. The Bundle object for the newly or previously installed bundle is returned.
+        return bundle
     }
 }
--- orsig-framework/src/main/groovy/com/github/oreissig/orsig/ORSiGFramework.groovy
+++ orsig-framework/src/main/groovy/com/github/oreissig/orsig/ORSiGFramework.groovy
@@ -8,28 +8,51 @@ import org.osgi.framework.FrameworkEvent
 import org.osgi.framework.FrameworkListener
 import org.osgi.framework.launch.Framework
 
+import com.google.common.eventbus.EventBus
+
 @CompileStatic
 class ORSiGFramework extends AbstractBundle implements Framework {
+    private EventBus bus = null
+    
     @Override
     void start(int options = 0) {
-        // 1.If this Framework is not in the STARTING state, initialize this Framework.
+        // 1. If this Framework is not in the STARTING state, initialize this Framework.
         if (state != Bundle.STARTING)
             init()
-        // TODO 2.All installed bundles must be started in accordance with each bundle's persistent autostart setting. This means some bundles will not be started, some will be started with eager activation and some will be started with their declared activation policy. The start level of this Framework is moved to the start level specified by the beginning start level framework property, as described in the Start Level Specification. If this framework property is not specified, then the start level of this Framework is moved to start level one (1). Any exceptions that occur during bundle starting must be wrapped in a BundleException and then published as a framework event of type FrameworkEvent.ERROR
-        // 3.This Framework's state is set to ACTIVE.
+        // TODO 2. All installed bundles must be started in accordance with each bundle's persistent autostart setting. This means some bundles will not be started, some will be started with eager activation and some will be started with their declared activation policy. The start level of this Framework is moved to the start level specified by the beginning start level framework property, as described in the Start Level Specification. If this framework property is not specified, then the start level of this Framework is moved to start level one (1). Any exceptions that occur during bundle starting must be wrapped in a BundleException and then published as a framework event of type FrameworkEvent.ERROR
+        // 3. This Framework's state is set to ACTIVE.
         state = Bundle.ACTIVE
-        // TODO 4.A framework event of type FrameworkEvent.STARTED is fired
+        // 4. A framework event of type FrameworkEvent.STARTED is fired
+        bus.post new FrameworkEvent(FrameworkEvent.STARTED, this, null)
     }
     
     @Override
     void stop(int options = 0) throws BundleException {
-        // TODO this is just a bare minimum
+        // 1.This Framework's state is set to STOPPING.
+        state = Bundle.STOPPING
+        // TODO 2. All installed bundles must be stopped without changing each bundle's persistent autostart setting. The start level of this Framework is moved to start level zero (0), as described in the Start Level Specification. Any exceptions that occur during bundle stopping must be wrapped in a BundleException and then published as a framework event of type FrameworkEvent.ERROR
+        // TODO 3. Unregister all services registered by this Framework.
+        // 4. Event handling is disabled.
+        bus = null
+        // 5. This Framework's state is set to RESOLVED.
         state = Bundle.RESOLVED
+        // TODO 6. All resources held by this Framework are released. This includes threads, bundle class loaders, open files, etc.
+        // TODO 7. Notify all threads that are waiting at waitForStop that the stop operation has completed.
     }
     
     @Override
     void init(FrameworkListener... listeners = []) throws BundleException {
-        // TODO do nothing for now
+        // TODO • Have generated a new framework UUID. 
+        // • Be in the STARTING state.
+        state = Bundle.STARTING
+        // TODO • Have a valid Bundle Context.
+        // TODO • Be at start level 0.
+        // • Have event handling enabled.
+        bus = new EventBus(getClass().name)
+        // TODO • Have reified Bundle objects for all installed bundles.
+        // TODO • Have registered any framework services. For example, ConditionalPermissionAdmin.
+        // TODO • Be adaptable to the OSGi defined types to which a system bundle can be adapted.
+        // TODO • Have called the start method of the extension bundle activator for all resolved extension bundles.
     }
     
     @Override
